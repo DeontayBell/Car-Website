1d18849cf4b8d86d669dd1c93e92544c48760592
Ethan <ethanwilson89@gmail.com>
2023-07-24 16:14:23 -0600

    README complete

---
+++ service/api/service_rest/encoders.py
@@ -0,0 +1,25 @@
+from common.json import ModelEncoder
+from .models import Technician, Appointment
+
+
+class TechnicianEncoder(ModelEncoder):
+    model = Technician
+    properties = [
+        "id",
+        "first_name",
+        "last_name",
+        "employee_id",
+    ]
+
+
+class AppointmentEncoder(ModelEncoder):
+    model = Appointment
+    properties = [
+        "id",
+        "date_time",
+        "reason",
+        "status",
+        "vin",
+        "customer",
+        "technician",
+    ]
--- service/api/service_rest/views.py
+++ service/api/service_rest/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.decorators.http import require_http_methods
+import json
 
-# Create your views here.
+from .encoders import (
+    TechnicianEncoder,
+    AppointmentEncoder,
+)
+
+from .models import Technician, AutomobileVO, Appointment
